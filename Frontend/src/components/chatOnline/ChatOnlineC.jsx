e8738b3c9593fa5a9066a054b91d572536063c6c
Priyam Maini <priyam9maini@gmail.com>
2024-05-27 18:33:44 +0530

    resolved minor bug

--- Frontend/src/components/chatOnline/ChatOnlineC.jsx
+++ Frontend/src/components/chatOnline/ChatOnlineC.jsx
@@ -4,25 +4,32 @@ import axios from 'axios';
 import { AuthContext } from '../../context/AuthContext';
 
 const ChatOnlineC = ({ currentId, setCurrentChat }) => {
-  const [user, setUser] = useState(null);
-  const { user: currentUser } = useContext(AuthContext);
-  const eventId = currentUser.events[0]; // Assuming currentUser.events is an array of event IDs
+  const [users, setUsers] = useState([]);
+  const { user } = useContext(AuthContext);
+  const eventId = user.events[0]; // Assuming user.events is an array of event IDs
 
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await axios.get(`/api/user/participants?eventId=${eventId}&userId=${currentUser._id}`);
-        setUser(res.data);
+        const res = await axios.get(`/api/user/participants?eventId=${eventId}&userId=${user._id}`);
+        // Check if the response data is an array
+        if (Array.isArray(res.data)) {
+          setUsers(res.data);
+        } else if (typeof res.data === 'object') {
+          // Convert the object to an array
+          setUsers([res.data]);
+        }
       } catch (error) {
-        console.error('Error fetching user:', error);
+        console.error('Error fetching users:', error);
       }
     };
     fetchUsers();
-  }, []);
+  }, [eventId, user._id]);
+  
 
-  const startConversation = async () => {
+  const startConversation = async (userId) => {
     try {
-      const res = await axios.get(`/api/conversations/find/${currentUser._id}/${user._id}`);
+      const res = await axios.get(`/api/conversations/find/${user._id}/${userId}`);
       setCurrentChat(res.data);
     } catch (error) {
       console.log('Error starting conversation:', error);
@@ -31,17 +38,21 @@ const ChatOnlineC = ({ currentId, setCurrentChat }) => {
 
   return (
     <div className="chatOnline">
-      <h3 className="chatOnlineHeader">{currentUser.role === 'HOST' ? 'Online Vendors' : 'Host'}</h3>
+      <h3 className="chatOnlineHeader">{user.role === 'HOST' ? 'Online Vendors' : 'Hosts'}</h3>
       <div className="chatOnlineFriends">
-        {user && (
-          <div className="chatOnlineFriend" onClick={startConversation}>
+        {users.map((user) => (
+          <div
+            key={user._id}
+            className="chatOnlineFriend"
+            onClick={() => startConversation(user._id)}
+          >
             <div className="chatOnlineImgContainer">
               {/* You can include user's image here if available */}
               <div className="chatOnlineBadge"></div>
             </div>
             <span className="chatOnlineName">{user.name}</span>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
