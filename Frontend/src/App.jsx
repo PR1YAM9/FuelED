4f3ba0411f2e8e2df679187b22c5d0f93c8d52d9
laiba ahsan <laiba.ahsan27@gmail.com>
2024-05-28 04:09:22 +0530

    Sidebar created

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,24 +1,37 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import { useContext } from "react";
 import { AuthContext } from "./context/AuthContext";
 import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import "./App.css";
-import Messenger from "./pages/messanger/Messanger"; 
-import Register from "./pages/register/Register"; 
+import Messenger from "./pages/messanger/Messanger";
+import Register from "./pages/register/Register";
 import RSVP from "./pages/RSVP";
-
+import SeatingPlan from "./pages/DashboardPages/SeatingPlan";
 function App() {
   const { user } = useContext(AuthContext);
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
+        {/* <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} /> */}
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-        <Route path="/register" element={user ? <Navigate to="/" /> : <Register />} />
-        <Route path="/messenger" element={user ? <Messenger /> : <Navigate to="/login" />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route
+          path="/messenger"
+          element={user ? <Messenger /> : <Navigate to="/login" />}
+        />
         <Route path="/rsvp" element={<RSVP />} />
+        <Route path="/dashboard/seating-plan" element={<SeatingPlan />} />
       </Routes>
     </Router>
   );
