2c1aa2cd3f9ec6102d5f214387d571e0dc9b1fb3
Priyam Maini <priyam9maini@gmail.com>
2024-05-31 01:54:00 +0530

    trying

--- Frontend/src/ApiCalls.jsx
+++ Frontend/src/ApiCalls.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const loginCall = async (userCredentials, dispatch) => {
     dispatch({ type: "LOGIN_START" });
     try {
-        const res = await axios.post("/api/auth/signin", userCredentials);
+        const res = await axios.post("https://fuel-ed-noyz.vercel.app/api/auth/signin", userCredentials);
         dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
     } catch (err) {
         dispatch({ type: "LOGIN_FAILURE", payload: err });
