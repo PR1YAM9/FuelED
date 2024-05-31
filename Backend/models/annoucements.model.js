6577265c26929e21b8d8ecf7c65d0688d0910d5c
Priyam Maini <priyam9maini@gmail.com>
2024-05-31 19:56:18 +0530

    added make and get annoucements backend

---
+++ Backend/models/annoucements.model.js
@@ -0,0 +1,28 @@
+// models/announcement.model.js
+import mongoose from "mongoose";
+
+const AnnouncementSchema = new mongoose.Schema(
+  {
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Event",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps: true }
+);
+
+const Announcement = mongoose.model("Announcement", AnnouncementSchema);
+export default Announcement;
