8bd997404489f8cd530f689016d47ea1ee4e8865
Priyam Maini <priyam9maini@gmail.com>
2024-06-02 21:46:05 +0530

    fix

--- Frontend/src/pages/RSVP/index.jsx
+++ Frontend/src/pages/RSVP/index.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useRef, useState, useEffect } from "react";
+import React, { useRef } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Navbar from "../../components/Navbar";
@@ -7,64 +7,49 @@ import defaultPicture from "../../assets/default_RSVP_Picture.png";
 import defaultPictureBg from "../../assets/default_RSVP_Picture_Bg.svg";
 import RsvpForm from "../../components/RsvpForm";
 import Footer from "../../components/Footer";
-import { AuthContext } from "../../context/AuthContext";
-import axios from "axios";
 
 export default function RSVP() {
   const formRef = useRef(null); // Create a ref for the form
-  const { user } = useContext(AuthContext);
-  const eventId = user.events[0]; // Hardcoded event ID for now
-  const [eventData, setEventData] = useState(null);
 
-  useEffect(() => {
-    const fetchEventDetails = async () => {
-      try {
-        const response = await axios.get(
-          `https://fuel-ed-noyz.vercel.app/api/event/geteventdetails/${eventId}`
-        );
-        setEventData(response.data.event);
-      } catch (error) {
-        console.error("Error fetching event details:", error);
-      }
-    };
-
-    fetchEventDetails();
-  }, []);
+  const data = [
+    {
+      imgUrl: defaultPicture,
+      Heading: "Invite to event kindly RSVP",
+      SubHeading:
+        "To mark special day, we are throwing a grand party and would love for you to join us in celebrating this special milestone. This event promises to be a delightful evening filled with laughter, joy, and cherished memories. We have planned an array of exciting activities, delectable food, and great music to make this night truly unforgettable. Your presence would add a touch of warmth and happiness to the celebration, making it even more special for us",
+    },
+  ];
 
   const handleScrollToForm = () => {
     formRef.current.scrollIntoView({ behavior: "smooth" });
   };
 
-  if (!eventData) {
-    return <div>Loading...</div>;
-  }
-
   return (
     <Box sx={{ backgroundColor: "#E09BAC" }}>
       <Navbar bgColor="#E09BAC" />
       <Box sx={{ padding: { md: "50px 25px", xs: "20px" } }}>
-        <Box
-          sx={{
-            marginBottom: "20px",
-            display: "flex",
-            flexDirection: { md: "row", xs: "column-reverse" },
-            justifyContent: "space-between",
-            position: "relative",
-            padding: { md: "80px 60px", xs: "20px 0" },
-            gap: "20px",
-          }}
-        >
+        {data.map((item, index) => (
           <Box
+            key={index}
             sx={{
+              marginBottom: "20px",
               display: "flex",
-              flexDirection: "column",
+              flexDirection: { md: "row", xs: "column-reverse" },
+              justifyContent: "space-between",
+              position: "relative",
+              padding: { md: "80px 60px", xs: "20px 0" },
               gap: "20px",
-              width: { md: "800px", xs: "auto" },
-              alignItems: "flex-start",
             }}
           >
-            {/* Conditional rendering for event name */}
-            {eventData.name && (
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+                width: { md: "800px", xs: "auto" },
+                alignItems: "flex-start",
+              }}
+            >
               <Typography
                 variant="h5"
                 sx={{
@@ -73,11 +58,8 @@ export default function RSVP() {
                   color: "white",
                 }}
               >
-                {eventData.name}
+                {item.Heading}
               </Typography>
-            )}
-            {/* Conditional rendering for event description */}
-            {eventData.description && (
               <Typography
                 variant="subtitle1"
                 sx={{
@@ -86,60 +68,60 @@ export default function RSVP() {
                   color: "white",
                 }}
               >
-                {eventData.description}
+                {item.SubHeading}
               </Typography>
-            )}
-            <Button
-              variant="contained"
-              onClick={handleScrollToForm}
-              sx={{
-                backgroundColor: "#C3A8E1",
-                fontFamily: "Imprima",
-                fontSize: { md: "29px", xs: "20px" },
-                borderRadius: "30px",
-                padding: "0px 40px",
-                "&:hover": {
+              <Button
+                variant="contained"
+                onClick={handleScrollToForm}
+                sx={{
                   backgroundColor: "#C3A8E1",
-                },
-                mt: { md: "30px", xs: "15px" },
-                color: "white",
-              }}
-            >
-              RSVP
-            </Button>
+                  fontFamily: "Imprima",
+                  fontSize: { md: "29px", xs: "20px" },
+                  borderRadius: "30px",
+                  padding: "0px 40px",
+                  "&:hover": {
+                    backgroundColor: "#C3A8E1",
+                  },
+                  mt: { md: "30px", xs: "15px" },
+                  color: "white",
+                }}
+              >
+                RSVP
+              </Button>
+            </Box>
+            <div style={{ position: "relative", margin: "0 auto" }}>
+              <Box
+                component="img"
+                src={defaultPictureBg}
+                alt="Background"
+                sx={{
+                  width: { md: "384px", xs: "250px" },
+                  height: { md: "432px", xs: "281px" },
+                  objectFit: "cover",
+                  position: "relative",
+                  zIndex: 1,
+                  mt: { md: "0", xs: "40px" },
+                }}
+              />
+              <Box
+                component="img"
+                src={item.imgUrl}
+                alt={item.Heading}
+                sx={{
+                  width: { md: "300px", xs: "200px" },
+                  height: { md: "300px", xs: "200px" },
+                  objectFit: "contain",
+                  position: "absolute",
+                  top: { md: "30px", xs: "15px" },
+                  left: { md: "10px", xs: "5px" },
+                  zIndex: 2,
+                  padding: { md: "30px", xs: "15px" },
+                  mt: { md: "0", xs: "40px" },
+                }}
+              />
+            </div>
           </Box>
-          <div style={{ position: "relative", margin: "0 auto" }}>
-            <Box
-              component="img"
-              src={defaultPictureBg}
-              alt="Background"
-              sx={{
-                width: { md: "384px", xs: "250px" },
-                height: { md: "432px", xs: "281px" },
-                objectFit: "cover",
-                position: "relative",
-                zIndex: 1,
-                mt: { md: "0", xs: "40px" },
-              }}
-            />
-            <Box
-              component="img"
-              src={defaultPicture}
-              alt={eventData.name}
-              sx={{
-                width: { md: "300px", xs: "200px" },
-                height: { md: "300px", xs: "200px" },
-                objectFit: "contain",
-                position: "absolute",
-                top: { md: "30px", xs: "15px" },
-                left: { md: "10px", xs: "5px" },
-                zIndex: 2,
-                padding: { md: "30px", xs: "15px" },
-                mt: { md: "0", xs: "40px" },
-              }}
-            />
-          </div>
-        </Box>
+        ))}
       </Box>
       <Box ref={formRef} sx={{ pt: 5, pb: 5 }}>
         <RsvpForm />
